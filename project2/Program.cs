0dd8565fa30855bfa5506eedb1a883cb7059b32e
BoldizsDavid <BoldizsDavid>
2016-08-04 15:34:00 +0200

    shapes

---
+++ project2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: boldizsdavid
+ * Date: 2016.08.04.
+ * Time: 10:06
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace project2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			shapes s = new shapes();
+			s.DoShaping();
+
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ project2/shapes.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+
+namespace project2
+{
+	
+	public class shapes
+	{
+		double side; double breadth; double length; double radius; double height; double breadthfortriangle;
+		public void DoShaping()
+		{
+        shapes c = new shapes();
+         while (true)
+         {
+        Console.WriteLine("Select Shape: (R)ectangle, (C)ircle, (S)quare, (T)riangle:");
+        var key = Console.ReadKey();
+       
+        
+        switch (key.Key)
+        {
+            case ConsoleKey.R:
+                c.Rectangle();
+                break;
+            case ConsoleKey.C:
+                c.Circle();
+                break;
+            case ConsoleKey.S:
+                c.Square();
+                break;
+            case ConsoleKey.T:
+                c.Triangle();
+                break;
+        }
+         }}
+         
+	   public void Rectangle()  
+        {  
+            Console.WriteLine("Enter the Length for Rectangle");  
+            length = double.Parse(Console.ReadLine());  
+            Console.WriteLine("Enter the breadth for Rectangle");  
+            breadth = double.Parse(Console.ReadLine());  
+
+            Console.WriteLine("Area of rectangle is :{0}", length * breadth);  
+        }  
+        public void Circle()  
+        {  
+            Console.WriteLine("Enter the Radius of the Circle");  
+            radius = double.Parse(Console.ReadLine());  
+
+            Console.WriteLine("Area of Circle is:{0}", 3.14 * radius * radius);  
+        }  
+        public void Square()  
+        {  
+            Console.WriteLine("Enter the side of a square");  
+            side = double.Parse(Console.ReadLine());  
+
+            Console.WriteLine("Area of Square is:{0}", side * side);  
+        }  
+         public void Triangle()  
+        {  
+            Console.WriteLine("Enter the Breadth for Triangle ");  
+            breadthfortriangle = double.Parse(Console.ReadLine());  
+            Console.WriteLine("Enter the Hieght for Triangle ");  
+            height = double.Parse(Console.ReadLine());    
+            Console.WriteLine("Area of Triangle is:{0}", (breadthfortriangle * height) / 2);  
+        }  
+		
+		}
+	}
