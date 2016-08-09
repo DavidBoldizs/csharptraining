eea2ebe05fce5cf5064c87b82506b6bcf533ca80
BoldizsDavid <BoldizsDavid>
2016-08-09 16:10:43 +0200

    könyv3

---
+++ masodik/masodik/hossz.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace masodik
+{
+	
+	public class hossz
+	{
+		static int maximum(int x,int y)
+{
+return (x>y?x:y);
+}
+public void GetLongest()
+{
+Console.WriteLine("Leghosszabb :");
+Console.WriteLine(maximum(4,3));
+}
+	}
+}
